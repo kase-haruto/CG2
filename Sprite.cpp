f2e340b8f5880a6d9c59a7bd5a925ef1e8aca43e
kase-haruto <0te06559726481t@au.com>
2024-06-12 12:48:14 +0900

    spriteのindexでの描画

--- Sprite.cpp
+++ Sprite.cpp
@@ -5,6 +5,8 @@
 #include"imgui.h"
 #include"TextureManager.h"
 
+#include<stdint.h>
+
 Sprite::Sprite(DirectXCommon* dx) :dxCommon_(dx){
 
 }
@@ -36,31 +38,49 @@ void Sprite::Update(){
 void Sprite::Draw(){
 	commandList_->SetGraphicsRootDescriptorTable(3, TextureManager::GetInstance()->GetTextureSrvHandleGPU());
 	commandList_->IASetVertexBuffers(0, 1, &vertexBufferViewSprite);
+	commandList_->IASetIndexBuffer(&indexBufferView);
 	commandList_->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
 	commandList_->SetGraphicsRootConstantBufferView(1, transformResource_->GetGPUVirtualAddress());
-	commandList_->DrawInstanced(6, 1, 0, 0);
+	commandList_->DrawIndexedInstanced(6, 1, 0, 0, 0);
 }
 
 void Sprite::CreateBuffer(){
 	//頂点用リソース
-	vertexResource_ = CreateBufferResource(device_.Get(), sizeof(VertexData) * 6);
+	vertexResource_ = CreateBufferResource(device_.Get(), sizeof(VertexData) * 4);
 	//頂点バッファビューを作成	
 	//リソースの先頭アドレスから使用する
 	vertexBufferViewSprite.BufferLocation = vertexResource_->GetGPUVirtualAddress();
 	//使用するリソースsizeは頂点6つ分size
-	vertexBufferViewSprite.SizeInBytes = sizeof(VertexData) * 6;
+	vertexBufferViewSprite.SizeInBytes = sizeof(VertexData) * 4;
 	//1頂点当たりのサイズ
 	vertexBufferViewSprite.StrideInBytes = sizeof(VertexData);
+	
 	//transformtionMatrix用のリソース
 	transformResource_ = CreateBufferResource(device_.Get(), sizeof(TransformationMatrix));
 	//マテリアル用リソース
 	materialResource_ = CreateBufferResource(device_.Get(), sizeof(Material));
+
+	//index用リソース
+	indexResource_ = CreateBufferResource(device_.Get(), sizeof(uint32_t) * 6);
+	//リソースの先頭アドレスから使う
+	indexBufferView.BufferLocation = indexResource_->GetGPUVirtualAddress();
+	indexBufferView.SizeInBytes = sizeof(uint32_t) * 6;
+	indexBufferView.Format = DXGI_FORMAT_R32_UINT;
 }
 
 void Sprite::Map(){
+	IndexResourceMap();
 	VertexResourceMap();
 	TransformResourceMap();
 	MaterialResourceMap();
+}
+
+void Sprite::IndexResourceMap(){
+	uint32_t* indexData = nullptr;
+	indexResource_->Map(0,nullptr,reinterpret_cast<void**>(&indexData));
+	indexData[0] = 0; indexData[1] = 1; indexData[2] = 2;
+	indexData[3] = 1; indexData[4] = 3; indexData[5] = 2;
+	indexResource_->Unmap(0,nullptr);
 }
 
 void Sprite::VertexResourceMap(){
@@ -76,31 +96,27 @@ void Sprite::VertexResourceMap(){
 	vertexData[1].normal = {0.0f,0.0f,-1.0f};
 	vertexData[2].position = {640.0f,360.0f,0.0f,1.0f};//左下
 	vertexData[2].texcoord = {1.0f,1.0f};
+	vertexData[2].normal = {0.0f,0.0f,-1.0f};
+	
+	vertexData[3].position = {640.0f,0.0f,0.0f,1.0f};//左下
+	vertexData[3].texcoord = {1.0f,0.0f};
 	vertexData[3].normal = {0.0f,0.0f,-1.0f};
 
-	//2枚目の三角形
-	vertexData[3].position = {0.0f,0.0f,0.0f,1.0f};//左下
-	vertexData[3].texcoord = {0.0f,0.0f};
-	vertexData[3].normal = {0.0f,0.0f,-1.0f};
-	vertexData[4].position = {640.0f,0.0f,0.0f,1.0f};//左下
-	vertexData[4].texcoord = {1.0f,0.0f};
-	vertexData[4].normal = {0.0f,0.0f,-1.0f};
-	vertexData[5].position = {640.0f,360.0f,0.0f,1.0f};//左下
-	vertexData[5].texcoord = {1.0f,1.0f};
-	vertexData[5].normal = {0.0f,0.0f,-1.0f};
-
+	vertexResource_->Unmap(0, nullptr);
 }
 
 void Sprite::TransformResourceMap(){
 	transformResource_->Map(0, nullptr, reinterpret_cast< void** >(&transformData));
 	//単位行列を書き込んでおく
 	*transformData = Matrix4x4::MakeIdentity();
+	transformResource_->Unmap(0, nullptr);
 }
 
 void Sprite::MaterialResourceMap(){
 	materialResource_->Map(0, nullptr, reinterpret_cast< void** >(&materialData_));
 	materialData_->color = {1.0f,1.0f,1.0f,1.0f};
 	materialData_->enableLighting = false;
+	materialResource_->Unmap(0, nullptr);
 }
 
 
