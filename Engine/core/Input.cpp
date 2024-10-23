56ad11489941b1aca1ca5b38d08c996779b8ca0d
kase-haruto <0te06559726481t@au.com>
2024-10-23 11:44:31 +0900

    マウスの機能を追加しました

--- Engine/core/Input.cpp
+++ Engine/core/Input.cpp
@@ -48,11 +48,15 @@ void Input::Finalize(){
 
 void Input::DirectInputInitialize(){
 	HRESULT hr;
+
 	hr = DirectInput8Create(
 		System::GetHinstance(), DIRECTINPUT_VERSION, IID_IDirectInput8,
 		( void** ) &directInput_, nullptr);
 	assert(SUCCEEDED(hr));
 
+	/*=======================================================================================
+				keyboad
+	========================================================================================*/
 	//キーボードデバイスの生成
 	hr = directInput_->CreateDevice(GUID_SysKeyboard, &keyboard_, NULL);
 	assert(SUCCEEDED(hr));
@@ -64,6 +68,24 @@ void Input::DirectInputInitialize(){
 	//排他制御レベルのセット
 	hr = keyboard_->SetCooperativeLevel(
 		System::GetHWND(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY
+	);
+	assert(SUCCEEDED(hr));
+
+
+	/*=======================================================================================
+				mouse
+	========================================================================================*/
+	//デバイスの生成
+	hr = directInput_->CreateDevice(GUID_SysMouse, &mouse_, NULL);
+	assert(SUCCEEDED(hr));
+
+	//入力データ形式のセット
+	hr = mouse_->SetDataFormat(&c_dfDIMouse);
+	assert(SUCCEEDED(hr));
+
+	//排他制御レベルのセット
+	hr = mouse_->SetCooperativeLevel(
+		System::GetHWND(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE
 	);
 	assert(SUCCEEDED(hr));
 }
@@ -87,11 +109,49 @@ void Input::KeyboardUpdate(){
 	}
 }
 
-bool Input::PushKey(uint32_t keyNum) {
+bool Input::PushKey(uint32_t keyNum){
 	return instance_->key_[keyNum];
 }
 
-bool Input::TriggerKey(uint32_t keyNum) {
+bool Input::TriggerKey(uint32_t keyNum){
 	return instance_->key_[keyNum] && !instance_->keyPre_[keyNum];
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+/*										 マウス                                        */
+/////////////////////////////////////////////////////////////////////////////////////////
+
+void Input::MouseUpdate(){
+	mouseStatePre_ = mouseState_;
+	HRESULT hr = mouse_->GetDeviceState(sizeof(DIMOUSESTATE), &mouseState_);
+	if (FAILED(hr)){
+		hr = mouse_->Acquire();
+		while (hr == DIERR_INPUTLOST){
+			hr = mouse_->Acquire();
+		}
+	}
+
+	// マウスの座標更新（画面座標にマッピングする処理）
+	mousePos_.x += mouseState_.lX;
+	mousePos_.y += mouseState_.lY;
+
+	// ホイール量をfloatで更新
+	mouseWheel_ = static_cast< float >(mouseState_.lZ) / 120.0f; // 1段階のホイール移動を1.0fにする
+}
+
+bool Input::PushMouseButton(int button){
+	return instance_->mouseState_.rgbButtons[button] & 0x80;
+}
+
+bool Input::TriggerMouseButton(int button){
+	return (instance_->mouseState_.rgbButtons[button] & 0x80) &&
+		!(instance_->mouseStatePre_.rgbButtons[button] & 0x80);
+}
+
+Vector2 Input::GetMousePosition(){
+	return instance_->mousePos_;
+}
+
+float Input::GetMouseWheel(){
+	return instance_->mouseWheel_;
+}
--- Engine/core/Input.h
+++ Engine/core/Input.h
@@ -3,6 +3,8 @@
 #define DIRECTINPUT_VERSION 0x0800 
 #include <dinput.h>
 
+#include "Vector2.h"
+
 #include <wrl.h>
 #include <array>
 
@@ -13,72 +15,118 @@ using Microsoft::WRL::ComPtr;
 
 class Input{
 public:
-    static Input* GetInstance();
+	static Input* GetInstance();
 
-    Input(const Input&) = delete;
-    Input& operator=(const Input&) = delete;
+	Input(const Input&) = delete;
+	Input& operator=(const Input&) = delete;
 
-    static void Initialize();
+	static void Initialize();
 
-    static void Update();
+	static void Update();
 
-    static void Finalize();
+	static void Finalize();
 
-    //=======================================================================================
-    //      キーボード
+	//=======================================================================================
+	//      キーボード
 public:
-    /// <summary>
-    /// キーボードを押した処理
-    /// 長押し
-    /// </summary>
-    /// <param name="keyNum"></param>
-    /// <returns></returns>
-    static bool PushKey(uint32_t keyNum);
+	/// <summary>
+	/// キーボードを押した処理
+	/// 長押し
+	/// </summary>
+	/// <param name="keyNum"></param>
+	/// <returns></returns>
+	static bool PushKey(uint32_t keyNum);
 
-    /// <summary>
-    /// キーボードを押した処理
-    /// 短押し
-    /// </summary>
-    /// <param name="keyNum"></param>
-    /// <returns></returns>
-    static bool TriggerKey(uint32_t keyNum);
+	/// <summary>
+	/// キーボードを押した処理
+	/// 短押し
+	/// </summary>
+	/// <param name="keyNum"></param>
+	/// <returns></returns>
+	static bool TriggerKey(uint32_t keyNum);
 
-    //
-    //=======================================================================================
+	//
+	//=======================================================================================
 
-    //=======================================================================================
-    //      ゲームパッド
+
+	//=======================================================================================
+	//      マウス
+public:
+	/// <summary>
+	/// マウスボタンが押されたか
+	/// </summary>
+	/// <param name="button">マウスのボタン (0: 左, 1: 右, 2: 中央)</param>
+	/// <returns>ボタンが押された場合は true</returns>
+	static bool PushMouseButton(int button);
+
+	/// <summary>
+	/// マウスボタンの短押しを確認する
+	/// </summary>
+	/// <param name="button">マウスのボタン (0: 左, 1: 右, 2: 中央)</param>
+	/// <returns>短押しの場合は true</returns>
+	static bool TriggerMouseButton(int button);
+
+	/// <summary>
+	/// マウスの座標
+	/// </summary>
+	/// <returns></returns>
+	static Vector2 GetMousePosition();   // マウスの座標を取得
+
+	/// <summary>
+	/// マウスのホイール移動量を取得
+	/// </summary>
+	static float GetMouseWheel();
+
+	//
+	//=======================================================================================
+
+
+	//=======================================================================================
+	//      ゲームパッド
 public:
 
-    //
-    //=======================================================================================
+	//
+	//=======================================================================================
 
 
 private:
-    Input() = default;
-    ~Input();
+	Input() = default;
+	~Input();
 
-    /// <summary>
-    /// directInputの初期化
-    /// </summary>
-    void DirectInputInitialize();
+	/// <summary>
+	/// directInputの初期化
+	/// </summary>
+	void DirectInputInitialize();
 
-    /// <summary>
-    /// キーボードの更新
-    /// </summary>
-    void KeyboardUpdate();
+	/// <summary>
+	/// キーボードの更新
+	/// </summary>
+	void KeyboardUpdate();
+
+	void MouseUpdate();
 
 private:
-    //インスタンス
-    static Input* instance_;
+	//インスタンス
+	static Input* instance_;
 
-    ComPtr<IDirectInput8> directInput_ = nullptr;
+	ComPtr<IDirectInput8> directInput_ = nullptr;
 
-    /*------------------------
-        キーボード
-    ------------------------*/
-    ComPtr<IDirectInputDevice8> keyboard_ = nullptr;
-    std::array<BYTE, 256> key_;
-    std::array<BYTE, 256> keyPre_;
 
+private:
+	/*------------------------
+		キーボード
+	------------------------*/
+	ComPtr<IDirectInputDevice8> keyboard_ = nullptr;
+	std::array<BYTE, 256> key_;
+	std::array<BYTE, 256> keyPre_;
+
+private:
+	/*------------------------
+	  マウス
+	------------------------*/
+	ComPtr<IDirectInputDevice8> mouse_ = nullptr;
+	DIMOUSESTATE mouseState_;
+	DIMOUSESTATE mouseStatePre_;
+	Vector2 mousePos_;
+	float mouseWheel_ = 0.0f;
 };
