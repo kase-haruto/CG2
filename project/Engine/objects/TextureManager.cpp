c5ba1e28c0e9586c916d57974ea8ba66bae9e3eb
kase-haruto <0te06559726481t@au.com>
2025-04-22 11:29:03 +0900

    effect用のリソース追加

--- project/Engine/objects/TextureManager.cpp
+++ project/Engine/objects/TextureManager.cpp
@@ -9,60 +9,60 @@
 #include <cassert>
 
 TextureManager* TextureManager::GetInstance(){
-    static TextureManager instance;
-    return &instance;
+	static TextureManager instance;
+	return &instance;
 }
 
 
 void TextureManager::StartUpLoad(){
-    LoadTexture("uvChecker.png");
-    LoadTexture("MonsterBall.png");
-
+	LoadTexture("uvChecker.png");
+	LoadTexture("MonsterBall.png");
+	LoadTexture("flower.png");
 }
 
 D3D12_GPU_DESCRIPTOR_HANDLE TextureManager::GetSrvHandle(const std::string& textureName) const{
-    auto it = textures_.find(textureName);
-    if (it != textures_.end()){
-        return it->second.GetSrvHandle();
-    }
-    return {};
+	auto it = textures_.find(textureName);
+	if (it != textures_.end()){
+		return it->second.GetSrvHandle();
+	}
+	return {};
 }
 
 const std::unordered_map<std::string, Texture>& TextureManager::GetLoadedTextures() const{
-    return textures_;
+	return textures_;
 }
 
 void TextureManager::Initialize(ImGuiManager* imgui){
-    device_ = GraphicsGroup::GetInstance()->GetDevice();
-    imgui_ = imgui;
-    descriptorSizeSrv_ = device_->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+	device_ = GraphicsGroup::GetInstance()->GetDevice();
+	imgui_ = imgui;
+	descriptorSizeSrv_ = device_->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
 }
 
 void TextureManager::Finalize(){
-    for (auto& texture : textures_){
-        texture.second.~Texture();
-    }
-    device_.Reset();
+	for (auto& texture : textures_){
+		texture.second.~Texture();
+	}
+	device_.Reset();
 }
 
 D3D12_GPU_DESCRIPTOR_HANDLE TextureManager::LoadTexture(const std::string& filePath){
-    // テクスチャが既にロードされているか確認
-    if (textures_.find(filePath) != textures_.end()){
-        return textures_[filePath].GetSrvHandle();
-    }
+	// テクスチャが既にロードされているか確認
+	if (textures_.find(filePath) != textures_.end()){
+		return textures_[filePath].GetSrvHandle();
+	}
 
-    // 新しいテクスチャをロード
-    Texture texture(filePath);
-    texture.Load(device_.Get());
-    texture.Upload(device_.Get());
+	// 新しいテクスチャをロード
+	Texture texture(filePath);
+	texture.Load(device_.Get());
+	texture.Upload(device_.Get());
 
-    // シェーダリソースビューを作成
-    texture.CreateShaderResourceView(device_.Get());
+	// シェーダリソースビューを作成
+	texture.CreateShaderResourceView(device_.Get());
 
-    // SRVハンドルを取得し、マップに追加
-    D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle = texture.GetSrvHandle();
-    textures_[filePath] = std::move(texture);
+	// SRVハンドルを取得し、マップに追加
+	D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle = texture.GetSrvHandle();
+	textures_[filePath] = std::move(texture);
 
-    return gpuHandle;
+	return gpuHandle;
 }
 
