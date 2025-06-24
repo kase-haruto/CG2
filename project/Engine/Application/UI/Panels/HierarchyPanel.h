17bad5c1f34bd2aafa1c784e2926ec60a9a8c2f5
kase haruto <k023g0052@g.neec.ac.jp>
2025-06-24 09:17:08 +0900

    ヒエラルキーの修正

--- project/Engine/Application/UI/Panels/HierarchyPanel.h
+++ project/Engine/Application/UI/Panels/HierarchyPanel.h
@@ -22,6 +22,7 @@ public:
 	~HierarchyPanel() override = default;
 
 	void Render() override;
+	void ShowObjectRecursive(SceneObject* obj, int depth);
 	void ShowObjectRecursive(SceneObject* obj);
 	const std::string& GetPanelName() const override;
 
